b28c9373ab05be4c75054d658ed490e29578880b
Niklas Evertsson <niklas.evertsson@gmail.com>
2022-05-15 01:06:18 +0100

    Change to mouse control of paddle

--- src/paddle.rs
+++ src/paddle.rs
@@ -1,36 +1,37 @@
 use bevy::{
-    input::Input,
     math::{const_vec3, Vec3},
-    prelude::{KeyCode, Query, Res, Transform, With},
+    prelude::{info, EventReader, Query, Res, Transform, With},
+    window::{CursorMoved, Windows},
 };
 
-use crate::{components::Paddle, walls::*, TIME_STEP};
+use crate::{components::Paddle, walls::*};
 
 pub const PADDLE_HEIGHT: f32 = 60.0;
 pub const PADDLE_SIZE: Vec3 = const_vec3!([120.0, 20.0, 0.0]);
 
 const PADDLE_PADDING: f32 = 10.0;
-const PADDLE_SPEED: f32 = 500.0;
 
 pub fn move_paddle(
-    keyboard_input: Res<Input<KeyCode>>,
+    windows: Res<Windows>,
+    mut cursor_moved_events: EventReader<CursorMoved>,
     mut query: Query<&mut Transform, With<Paddle>>,
 ) {
-    let mut paddle_transform = query.single_mut();
-    let mut direction = 0.0;
-
-    if keyboard_input.pressed(KeyCode::Left) {
-        direction -= 1.0;
+    if cursor_moved_events.is_empty() {
+        return;
     }
 
-    if keyboard_input.pressed(KeyCode::Right) {
-        direction += 1.0;
-    }
+    let window = windows.primary();
+    let window_half_with = window.width() / 2.0;
 
-    let new_paddle_position = paddle_transform.translation.x + direction * PADDLE_SPEED * TIME_STEP;
+    let mut new_position: f32 = 0.0;
+    for event in cursor_moved_events.iter() {
+        info!("{:?}", event);
+        new_position = event.position.x - window_half_with;
+    }
 
     let left_bound = LEFT_WALL + WALL_THICKNESS / 2.0 + PADDLE_SIZE.x / 2.0 + PADDLE_PADDING;
     let right_bound = RIGHT_WALL - WALL_THICKNESS / 2.0 - PADDLE_SIZE.x / 2.0 - PADDLE_PADDING;
 
-    paddle_transform.translation.x = new_paddle_position.clamp(left_bound, right_bound);
+    let mut paddle_transform = query.single_mut();
+    paddle_transform.translation.x = new_position.clamp(left_bound, right_bound);
 }
